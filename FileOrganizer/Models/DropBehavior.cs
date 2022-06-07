29497aff298209f2eaf204eacd1894cadd00dcc5
Akn <sibata486@gmail.com>
2022-06-07 23:30:24 +0900

    add / ファイルドロップを受け付けるためのクラスを追加

---
+++ FileOrganizer/Models/DropBehavior.cs
@@ -0,0 +1,39 @@
+﻿namespace FileOrganizer.Models
+{
+    using System.Windows;
+    using FileOrganizer.ViewModels;
+    using Microsoft.Xaml.Behaviors;
+
+    public class DropBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // ファイルをドラッグしてきて、コントロール上に乗せた際の処理
+            AssociatedObject.PreviewDragOver += this.AssociatedObject_PreviewDragOver;
+
+            // ファイルをドロップした際の処理
+            AssociatedObject.Drop += this.AssociatedObject_Drop;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewDragOver -= this.AssociatedObject_PreviewDragOver;
+            AssociatedObject.Drop -= this.AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_Drop(object sender, DragEventArgs e)
+        {
+            // ファイルパスの一覧の配列
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+        }
+
+        private void AssociatedObject_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+    }
+}
