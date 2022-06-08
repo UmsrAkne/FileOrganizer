c1bdb67e5f2c89ce709e2219b6f13457903f7924
Akn <sibata486@gmail.com>
2022-06-09 00:38:09 +0900

    add / ２個１対のリストを扱うクラスを追加

---
+++ FileOrganizer/Models/DoubleFileList.cs
@@ -0,0 +1,21 @@
+﻿namespace FileOrganizer.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DoubleFileList
+    {
+        public DoubleFileList(List<ExtendFileInfo> files)
+        {
+            Files = files;
+            OriginalFiles = Files.ToList();
+        }
+
+        public List<ExtendFileInfo> Files { get; set; }
+
+        private List<ExtendFileInfo> OriginalFiles { get; set; }
+    }
+}
