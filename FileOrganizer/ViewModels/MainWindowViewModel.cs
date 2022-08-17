0db56132648eef2af107b281aa1cb9821e023fd6
Akn <sibata486@gmail.com>
2022-08-17 12:25:46 +0900

    fix / コマンドパラメーターが不要になったコマンドを修正

--- FileOrganizer/ViewModels/MainWindowViewModel.cs
+++ FileOrganizer/ViewModels/MainWindowViewModel.cs
@@ -96,7 +96,7 @@
             ReIndex();
         });
 
-        public DelegateCommand<ListView> ToggleIgnoreFileAndForwardCommand => new DelegateCommand<ListView>((lv) =>
+        public DelegateCommand ToggleIgnoreFileAndForwardCommand => new DelegateCommand(() =>
         {
             ToggleIgnoreFileCommand.Execute();
             CursorDownCommand.Execute();
@@ -111,24 +111,22 @@
             }
         });
 
-        public DelegateCommand<ListView> JumpToNextMarkCommand => new DelegateCommand<ListView>((lv) =>
+        public DelegateCommand JumpToNextMarkCommand => new DelegateCommand(() =>
         {
             var nextMark = ExtendFileInfos.Skip(SelectedFileIndex + 1).FirstOrDefault(f => f.Marked);
 
             if (nextMark != null)
             {
-                lv.ScrollIntoView(nextMark);
                 SelectedFileIndex = ExtendFileInfos.IndexOf(nextMark);
             }
         });
 
-        public DelegateCommand<ListView> JumpToPrevMarkCommand => new DelegateCommand<ListView>((lv) =>
+        public DelegateCommand JumpToPrevMarkCommand => new DelegateCommand(() =>
         {
             var prevMark = ExtendFileInfos.Take(SelectedFileIndex - 1).Reverse().FirstOrDefault(f => f.Marked);
 
             if (prevMark != null)
             {
-                lv.ScrollIntoView(prevMark);
                 SelectedFileIndex = ExtendFileInfos.IndexOf(prevMark);
             }
         });
