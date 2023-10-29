12c606004db67e6d07485c31dd6bde77041062a6
Akn <sibata486@gmail.com>
2023-10-30 01:19:59 +0900

    del / MainWindowViewModel の不要となったカウンタープロパティを削除

--- FileOrganizer/ViewModels/MainWindowViewModel.cs
+++ FileOrganizer/ViewModels/MainWindowViewModel.cs
@@ -22,9 +22,7 @@ namespace FileOrganizer.ViewModels
         private ObservableCollection<ExtendFileInfo> extendFileInfos = new ObservableCollection<ExtendFileInfo>();
         private ExtendFileInfo selectedItem;
         private bool ignoreFileIsVisible = true;
-        private int ignoreFileCount;
         private int maximumIndex;
-        private int markedFileCount;
         private double fontSize = 12.0;
         private int listViewItemLineHeight = 15;
 
@@ -96,10 +94,6 @@ namespace FileOrganizer.ViewModels
             Process.Start(SelectedItem.FileInfo.FullName);
         });
 
-        public int IgnoreFileCount { get => ignoreFileCount; set => SetProperty(ref ignoreFileCount, value); }
-
-        public int MarkedFileCount { get => markedFileCount; set => SetProperty(ref markedFileCount, value); }
-
         public int ListViewItemLineHeight
         {
             get => listViewItemLineHeight;
@@ -111,9 +105,6 @@ namespace FileOrganizer.ViewModels
         {
             FileContainer = new FileContainer(files);
             RaisePropertyChanged(nameof(FileContainer));
-
-            MarkedFileCount = 0;
-            IgnoreFileCount = 0;
         }
 
         private int GetDisplayingItemCount(double lvActualHeight)
