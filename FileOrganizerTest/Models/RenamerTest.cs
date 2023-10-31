ff5c24ee1dc7d04c8fda2f0bf2551f93282e35b2
Akn <sibata486@gmail.com>
2023-10-31 17:04:34 +0900

    add feat / AppendNumber() に開始番号を設定可能にした

--- FileOrganizerTest/Models/RenamerTest.cs
+++ FileOrganizerTest/Models/RenamerTest.cs
@@ -49,5 +49,25 @@ namespace FileOrganizerTest.Models
             Assert.That(files[2].FileInfo.Name, Is.EqualTo("0003_c.temp"));
             Assert.That(files[3].FileInfo.Name, Is.EqualTo("0004_d.temp"));
         }
+
+        [Test]
+        public void AppendNumberTest_開始番号設定()
+        {
+            var renamer = new Renamer();
+            var files = new List<ExtendFileInfo>()
+            {
+                new ExtendFileInfo($"{testDirectoryName}\\a.temp"),
+                new ExtendFileInfo($"{testDirectoryName}\\b.temp"),
+                new ExtendFileInfo($"{testDirectoryName}\\c.temp"),
+                new ExtendFileInfo($"{testDirectoryName}\\d.temp"),
+            };
+
+            renamer.AppendNumber(files, 2);
+
+            Assert.That(files[0].FileInfo.Name, Is.EqualTo("0002_a.temp"));
+            Assert.That(files[1].FileInfo.Name, Is.EqualTo("0003_b.temp"));
+            Assert.That(files[2].FileInfo.Name, Is.EqualTo("0004_c.temp"));
+            Assert.That(files[3].FileInfo.Name, Is.EqualTo("0005_d.temp"));
+        }
     }
 }
